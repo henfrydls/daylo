10a4845901439ae97f69c112937ecfd85c497b2f
Henfry De Los Santos <newswebstar@gmail.com>
2026-03-18 22:57:15 -0400

    S5-07d: Fix Rust formatting in lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,8 +6,7 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let builder = tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init());
+    let builder = tauri::Builder::default().plugin(tauri_plugin_shell::init());
 
     #[cfg(not(any(target_os = "android", target_os = "ios")))]
     let builder = builder.plugin(tauri_plugin_opener::init());
